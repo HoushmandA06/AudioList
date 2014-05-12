a49e0190da5c42814305f336f6f480ad800a440f
Ali Houshmand <ali.houshmand06@gmail.com>
2014-05-12 13:33:27 -0400

    minor chg

---
+++ AudioList/ALATrack.m
@@ -0,0 +1,22 @@
+//
+//  ALATrack.m
+//  AudioList
+//
+//  Created by Ali Houshmand on 5/12/14.
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import "ALATrack.h"
+
+@implementation ALATrack
+{
+    NSMutableDictionary * dict;
+}
+
++(id)newTrack
+{
+    return [[ALATrack alloc] init];
+}
+
+
+@end
